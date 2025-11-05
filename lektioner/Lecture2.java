e6bf4767b12dc55705ccaa62e5e3def80a4e8378
Ludvig Dahlgren <ludvig.dahlgren05@gmail.com>
2025-11-05 13:38:35 +0100

    changes

---
+++ lektioner/Lecture2.java
@@ -0,0 +1,13 @@
+public class Lecture2 {
+    public static void main(String[] args) { // 
+        int x = Testing();
+        System.out.println(x);
+        System.out.println(args[0]);
+    }
+
+    public static int Testing() {
+        int a = 5;
+        System.out.println("test");
+        return (a+a);
+    }
+}
